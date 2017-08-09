b351b6231d9f059f8f160f1d96d716141b571a0e
Tom Harrop <twharrop@gmail.com>
2017-08-09 12:41:36 +1200

    do contigs need to have the same names?

--- src/convert_wolbachia_genomes_to_fasta.py
+++ src/convert_wolbachia_genomes_to_fasta.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 from Bio import SeqIO
+import copy
 import gzip
 import tompytools
 
@@ -9,22 +10,31 @@ def read_gff_gz(gbf):
         gb = SeqIO.parse(f, 'gb')
         return(next(gb))
 
+def rename_contigs(sr):
+    my_seq_record = copy.deepcopy(sr)
+    my_seq_record.id = 'Wolbachia'
+    my_seq_record.name = ''
+    my_seq_record.description = ''
+    return(my_seq_record)
+
 def main():
     genbank_files = tompytools.find_all(['genomic.gbff.gz'], 'output/genomes')
     gb_list = [read_gff_gz(x) for x in genbank_files]
 
     main_contigs = [x for x in gb_list if x.name == 'NC_002978']
+    main_contigs_renamed = [rename_contigs(x) for x in main_contigs]
     alt_contigs = [x for x in gb_list if not x.name == 'NC_002978']
+    alt_contigs_renamed = [rename_contigs(x) for x in alt_contigs]
     main_fasta = 'output/genomes/wolbachia_wmel.fasta'
     alt_fasta = 'output/genomes/wolbachia_others.fasta'
 
     SeqIO.write(
-        sequences=main_contigs,
+        sequences=main_contigs_renamed,
         handle=main_fasta,
         format='fasta')
 
     SeqIO.write(
-        sequences=alt_contigs,
+        sequences=alt_contigs_renamed,
         handle=alt_fasta,
         format='fasta')
 
