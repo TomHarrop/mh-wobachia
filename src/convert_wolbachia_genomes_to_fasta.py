3b44eca2a08912a8dd155217090d8f63d9b076c3
Tom Harrop <twharrop@gmail.com>
2017-08-09 16:17:52 +1200

    all contigs with annotated alt

--- src/convert_wolbachia_genomes_to_fasta.py
+++ src/convert_wolbachia_genomes_to_fasta.py
@@ -12,7 +12,6 @@ def read_gff_gz(gbf):
 
 def rename_contigs(sr):
     my_seq_record = copy.deepcopy(sr)
-    my_seq_record.id = 'Wolbachia'
     my_seq_record.name = ''
     my_seq_record.description = ''
     return(my_seq_record)
@@ -21,12 +20,27 @@ def main():
     genbank_files = tompytools.find_all(['genomic.gbff.gz'], 'output/genomes')
     gb_list = [read_gff_gz(x) for x in genbank_files]
 
+    # separate main and ALT contigs
     main_contigs = [x for x in gb_list if x.name == 'NC_002978']
-    main_contigs_renamed = [rename_contigs(x) for x in main_contigs]
     alt_contigs = [x for x in gb_list if not x.name == 'NC_002978']
+
+    # rename contigs to remove descprtion and name
+    main_contigs_renamed = [rename_contigs(x) for x in main_contigs]
     alt_contigs_renamed = [rename_contigs(x) for x in alt_contigs]
+    all_contigs_renamed = [rename_contigs(x) for x in gb_list]
+
+    # generate list of ALT contig names
+    alt_contig_names = [x.id for x in alt_contigs_renamed]
+
+    # write fasta files
+    all_fasta = 'output/genomes/wolbachia_all.fasta'
     main_fasta = 'output/genomes/wolbachia_wmel.fasta'
     alt_fasta = 'output/genomes/wolbachia_others.fasta'
+
+    SeqIO.write(
+        sequences=all_contigs_renamed,
+        handle=all_fasta,
+        format='fasta')
 
     SeqIO.write(
         sequences=main_contigs_renamed,
@@ -37,6 +51,12 @@ def main():
         sequences=alt_contigs_renamed,
         handle=alt_fasta,
         format='fasta')
+
+    # write alt contig names
+    indexbase_alt = 'output/genomes/wolbachia_all.alt'
+    with open(indexbase_alt, 'wt') as f:
+        f.writelines('\n'.join(alt_contig_names))
+        f.write('\n')
 
 if __name__ == '__main__':
     main()
